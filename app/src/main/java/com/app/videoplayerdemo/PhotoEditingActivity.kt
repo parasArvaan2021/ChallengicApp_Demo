d285ecd15436cf4e318d911f6daf722375c3788c
paras kansagara <91609065+parasArvaan2021@users.noreply.github.com>
2022-06-01 19:34:01 +0530

    image to video convertor code

--- app/src/main/java/com/app/videoplayerdemo/PhotoEditingActivity.kt
+++ app/src/main/java/com/app/videoplayerdemo/PhotoEditingActivity.kt
@@ -2,18 +2,28 @@ package com.app.videoplayerdemo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.util.Size
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import com.bumptech.glide.Glide
+import com.daasuu.imagetovideo.EncodeListener
+import com.daasuu.imagetovideo.ImageToVideoConverter
 import com.xlythe.view.camera.VideoView
 import java.io.File
-import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class PhotoEditingActivity : AppCompatActivity() {
     lateinit var imagePreview: AppCompatImageView
-    lateinit var videoView: VideoView
+    private lateinit var videoView: VideoView
+    private var path: String? = ""
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +32,75 @@ class PhotoEditingActivity : AppCompatActivity() {
 
         imagePreview = findViewById(R.id.imagePreview)
         videoView = findViewById(R.id.videoView)
-        val path = intent.getStringExtra("IMAGE_PATH")
+        path = try {
+            intent.getStringExtra("IMAGE_PATH")
+        } catch (ex: Exception) {
+            ""
+        }
+
         Log.e("TAG", "onCreate: $path")
 
-        if (path != null && path.contains(".mp4")) {
-            videoView.visibility=View.VISIBLE
-            videoView.setVolume(0.3f)
-            videoView.file = File(path)
-            videoView.play()
-        } else
-            Glide.with(this).load(path).into(imagePreview)
+        findViewById<AppCompatButton>(R.id.btnConvertImgToVideo).setOnClickListener {
+
+            if (path != null)
+                convertImageToVideo()
+        }
+
+        findViewById<AppCompatButton>(R.id.btnShowPreview).setOnClickListener {
+            findViewById<LinearLayoutCompat>(R.id.llButton).visibility = View.GONE
+            if (path != null && path!!.contains(".mp4")) {
+                videoView.visibility = View.VISIBLE
+                videoView.setVolume(0.3f)
+                videoView.file = File(path)
+                videoView.play()
+            } else {
+                imagePreview.visibility = View.VISIBLE
+                Glide.with(this).load(path).into(imagePreview)
+            }
+        }
+
+    }
+
+    private fun convertImageToVideo() {
+        val imageToVideo = ImageToVideoConverter(
+            outputPath = storeVideoPath(),
+            inputImagePath = (if (path != null && !path!!.contains(".mp4")) path else "").toString(),
+            size = Size(1024, 1024),
+            duration = TimeUnit.SECONDS.toMicros(15),
+            listener = object : EncodeListener {
+                override fun onCompleted() {
+                    runOnUiThread {
+                        Log.e("TAG", "onCompleted: ")
+                        Toast.makeText(this@PhotoEditingActivity, "Complete", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                override fun onFailed(exception: Exception) {
+                    Log.e("TAG", "onFailed: ")
+
+                }
+
+                override fun onProgress(progress: Float) {
+                    runOnUiThread {
+                        Log.e("TAG", "onProgress: $progress")
+                    }
+
+
+                }
+
+            }
+        )
+
+        imageToVideo.start()
+    }
+
+    private fun getAndroidMoviesFolder(): File {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
+    }
+
+    private fun storeVideoPath(): String {
+        return getAndroidMoviesFolder().absolutePath + "/" + SimpleDateFormat("yyyyMM_dd-HHmmss").format(Date()) + "image_video.mp4"
+
     }
 }
