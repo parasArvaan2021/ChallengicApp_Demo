34b5c76199088b259c4054abfce6a2c5f440ba98
paras kansagara <91609065+parasArvaan2021@users.noreply.github.com>
2022-06-02 13:00:45 +0530

    changes

--- app/src/main/java/com/app/videoplayerdemo/BottomSheetDialog.kt
+++ app/src/main/java/com/app/videoplayerdemo/BottomSheetDialog.kt
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
-
-class BottomSheetDialog: BottomSheetDialogFragment() {
+class BottomSheetDialog : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return  inflater.inflate(R.layout.bottom_sheet_layout,container,false)
+        return inflater.inflate(R.layout.bottom_sheet_layout, container, false)
     }
 }
